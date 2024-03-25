546480891dbf0d77af1aae5de0b7d6a6c433fb9a
Jayden Tyson <jayden@Jaydens-MacBook-Pro.local>
2024-03-25 11:18:55 -0500

    global variable and method for adding nodes

---
+++ network/src/components/AppWrapper.js
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext()
+export const AppData = () => useContext(AppContext)
+
+export const AppWrapper = ({children}) => {
+    const [nodes, setNodes] = useState([
+        {id: 1, label: "Jayden", shape: "circle", button: true},
+        {id: 2, label: "Ryan", shape: "box"},
+        {id: 3, label: "Amanda", shape: "box"},
+        {id: 4, label: "Steffanie", shape: "circle"},
+        {id: 5, label: "Aaron", shape: "circle"},
+        {id: 6, label: "Malachi", shape: "circle"},
+        {id: 7, label: "Zach", shape: "circle"},
+        {id: 8, label: "Aven", shape: "box"},
+        {id: 9, label: "Bitty", shape: "box"},
+        {id: 10, label: "Joya", shape: "box"}
+    ])
+    const [edges, setEdges] = useState([
+        {from: 1, to: 2},
+        {from: 2, to: 3},
+        {from: 1, to: 4},
+        {from: 1, to: 5},
+        {from: 1, to: 6},
+        {from: 1, to: 7},
+        {from: 3, to: 8},
+        {from: 3, to: 9},
+        {from: 4, to: 10}
+    ])
+
+    const addPerson = (name) => {
+        var id = nodes.length + 1
+        const newPerson = {id: id, label: name, shape: "box"};
+        const arrayCopy = [...nodes]; //creating a copy
+        arrayCopy.push(newPerson);
+        setNodes(arrayCopy);
+    };
+
+    return (
+        <AppContext.Provider value = {{nodes, edges, addPerson}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
--- network/src/components/graph.js
+++ network/src/components/graph.js
@@ -1,39 +1,11 @@
 import React, { useState } from 'react';
 import Graph from 'react-vis-network-graph';
 import { options } from './options';
-let nextNodeId = 11
-let nextEdgeId = 1
+import { AppData } from './AppWrapper';
+
 export default function GraphView() {
     const [name, setName] = useState('');
-    const [nodes, setNodes] = useState([
-        {id: 1, label: "Jayden", shape: "circle"},
-        {id: 2, label: "Ryan", shape: "box"},
-        {id: 3, label: "Amanda", shape: "box"},
-        {id: 4, label: "Steffanie", shape: "circle"},
-        {id: 5, label: "Aaron", shape: "circle"},
-        {id: 6, label: "Malachi", shape: "circle"},
-        {id: 7, label: "Zach", shape: "circle"},
-        {id: 8, label: "Aven", shape: "box"},
-        {id: 9, label: "Bitty", shape: "box"},
-        {id: 10, label: "Joya", shape: "box"}
-    ])
-    const [edges, setEdges] = useState([
-        {id: "e" + nextEdgeId++, from: 1, to: 2},
-        {id: "e" + nextEdgeId++, from: 2, to: 3},
-        {id: "e" + nextEdgeId++, from: 1, to: 4},
-        {id: "e" + nextEdgeId++, from: 1, to: 5},
-        {id: "e" + nextEdgeId++, from: 1, to: 6},
-        {id: "e" + nextEdgeId++, from: 1, to: 7},
-        {id: "e" + nextEdgeId++, from: 3, to: 8},
-        {id: "e" + nextEdgeId++, from: 3, to: 9},
-        {id: "e" + nextEdgeId++, from: 4, to: 10}
-    ])
-    const addPerson = () => {
-        const newPerson = {id: nextNodeId++, label: name, shape: "box"};
-        const arrayCopy = [...nodes]; //creating a copy
-        arrayCopy.push(newPerson);
-        setNodes(arrayCopy);
-    };
+    const { nodes, edges, addPerson } = AppData();
     var data = {nodes: nodes, edges: edges}
     return (
         <>
@@ -41,7 +13,7 @@ export default function GraphView() {
         value={name}
         onChange={e => setName(e.target.value)}
         />
-        <button className="addPersonButton" onClick={addPerson}>
+        <button className="addPersonButton" onClick={() => addPerson(name)}>
             Add Person
         </button>
         <div className='container'>
