66785fb221f4038be674ea2b22dea3c72bcb092e
simon <simon.dunk@okbu.edu>
2024-11-19 23:10:48 -0600

    Datatbase documentation

--- network/api/api.js
+++ network/api/api.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 app.post('/addNode', (req, res) => {
     // TODO: Add schema validation
-    const partition = "Partition" // Change to something else later
+    const partition = "Development" // TODO: Later will be implemented as a user
     const nodeID = req.body.nodeID;
     const data = req.body.data;
     const tableName = process.env.TABLE_NAME;
@@ -17,7 +17,7 @@ app.post('/addNode', (req, res) => {
 
 app.post("/deleteNode", async function (req, res) {
     // TODO: Add schema validation
-    const partition = "Partition" // Change to something else later
+    const partition = "Development" // TODO: Later will be implemented as a user
     const nodeID = req.body.nodeID;
     const tableName = process.env.TABLE_NAME;
     console.log(`:api-route /deleteNode ${partition}, ${nodeID}`)
@@ -29,10 +29,10 @@ app.post("/deleteNode", async function (req, res) {
 
 app.post('/updateNode', (req, res) => {
     // TODO: Add schema validation
-    const partition = "Partition" // Change to something else later
+    const partition = "Development" // TODO: Later will be implemented as a user
     const nodeID = req.body.nodeID;
     const data = req.body.data;
-    const tableName = process.env.TABLE_NAME;
+    const tableName = process.env.PERSONS_TABLE_NAME;
     console.log(`:api-route /updateNode ${partition}, ${nodeID}, ${data}, ${tableName}`)
     updateNode(partition, nodeID, data, tableName).then(function (returnVal) {
         console.log(":api-route /Returned " + returnVal);
